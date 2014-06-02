48d4acb26f0065dfb0baa4ac3d940254f2eedbc7
Paul Robson <paul@robsons.org.uk>
2014-06-02 08:08:01 +0100

    Fixed Bugs
    ShortEmitter auto started immediately. Added LoopingShortEmitter.

--- main.lua
+++ main.lua
@@ -1,3 +1,9 @@
 local Particle = require("system.particle")
 
-Particle.Emitter:new("stars"):start(display.contentWidth/2,display.contentHeight/2)
+local p = Particle.LoopingShortEmitter:new("stars",2000)
+print("create")
+
+print(p.cancel)
+timer.performWithDelay(1000,function() p:start(display.contentWidth/2,display.contentHeight/2) end)
+
+timer.performWithDelay(10000,function() p:cancel() end)
